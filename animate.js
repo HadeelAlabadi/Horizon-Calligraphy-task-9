6fef1fce25b465840bd717d8113bb7093822d9e9
HadeelAlabadi <163149057+HadeelAlabadi@users.noreply.github.com>
2024-11-05 18:41:27 +0300

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    window.location.href = "/Horizon Calligraphy/index.html"; // Change the name of the second page according to your page name
+}, 3000); // 3000 = 3 seconds
